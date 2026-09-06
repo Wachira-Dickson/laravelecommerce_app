4a3e0fcc547b7bb74d365ef9cbbd58a552b5fe66
Wachira-Dickson <dicksonwachira886@gmail.com>
2026-09-06 12:04:40 +0300

    Integrate admin template

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,13 +1,21 @@
-<x-app-layout>
-    
-</x-app-layout>
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title></title>
-</head>
-<body>
-    admin
-</body>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
+      <!-- partial -->
+      
+      @include('admin.navbar')
+        <!-- partial -->
+       @include('admin.body')
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+      @include('admin.script')
+    <!-- End custom js for this page -->
+  </body>
 </html>
